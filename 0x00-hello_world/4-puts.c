1f308e0075719f909de70c3a5e6a26075921d413
Farai Matsika <faramatsi2@gmail.com>
2023-03-17 22:56:47 +0000

    4 puts update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 /**
-* main - Entry point
-*   
-* Return: Always 0 (Success)
-*/
+  * main - this is the main function
+  * Return: always 0
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
