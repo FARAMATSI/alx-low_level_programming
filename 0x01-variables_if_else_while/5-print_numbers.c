105b65b929d0c96518a655aff72aa27437020663
Farai Matsika <faramatsi2@gmail.com>
2023-03-20 12:28:40 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - loop is printing the one digit numbers from 1 to 9
+*  
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i <= 9; i++)
+	printf("%d",i);
+	printf("\n");
+	return (0);
+}
