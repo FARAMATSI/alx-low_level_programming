f427ddeb8a2a77e9bf843db78e197fbf848321d5
Farai Matsika <faramatsi2@gmail.com>
2023-03-20 13:22:58 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* main - loop is printing the alphabet from z to a with lowercase
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	putchar(alpha);
+	printf("\n");
+	return (0);
+}
