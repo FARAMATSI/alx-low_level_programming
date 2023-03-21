9843a82cc7bc3931623738c830ef78c2a51c9fb1
Farai Matsika <faramatsi2@gmail.com>
2023-03-21 10:08:32 +0000

    Mass commit

---
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Prints.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char a[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+{
+	putchar(a[i]);
+}
+	putchar('\n');
+}
